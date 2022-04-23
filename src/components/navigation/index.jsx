4cc6db7c2aa8c832b8296582ae84c3bb7461771c
MUHESI <mvmmuhesi@gmail.com>
2022-04-23 23:25:48 +0200

    improve components style

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import './style.css'
-import { iconLogo, userImg, imgDev } from '../../constants';
- const NavBar = () => {
-    return (
-        <div className='mainNavbar'>
-        <div className='logo'>
-           <img src={iconLogo} alt="..." />
-        </div> 
-       <div className='contentUser'>
-         <div className='imgUser'>
-              <img src={userImg} alt="..." />
-              </div>
-         <div className='nameUser'>Muhesi Moses </div> 
-       </div>
+import React from "react";
+import "../../styles/styleNavBar.css";
+import { iconLogo, userImg } from "../../constants";
+const NavBar = () => {
+  return (
+    <div className='mainNavbar'>
+      <div className='logo'>
+        <img src={iconLogo} alt='...' />
       </div>
-    )
-}
+      <div className='contentUser'>
+        <div className='imgUser'>
+          <img src={userImg} alt='...' />
+        </div>
+        <div className='nameUser'>Muhesi Moses </div>
+      </div>
+    </div>
+  );
+};
 export default NavBar;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import ChargeFile from "../components/Input";
-// import ShowResult from '../components/showResult';
 import { imgDev } from "../constants/";
 import "../styles/styleHome.css";
 import "../styles/chargeFile.css";
@@ -8,22 +6,27 @@ import { handleUploadFiles } from "../utils/controllerUploadFile";
 import { postAPI } from "../utils/fetchData";
 
 const Home = () => {
+  // Initialize state
   const [image, setImage] = useState({
     uri: null,
     file: null
   });
-  const [resPred, setResPred] = useState({ attendu: "", prediction: "" });
+  const [isLoading, setIsLoading] = useState(false);
+  const [dataForm, setDataForm] = useState({
+    attendu: "",
+    prediction: "",
+    linkImg: ""
+  });
 
-  const loadImage = (e) => {
-    const URI = URL.createObjectURL(e.target.files[0]);
-    setImage({ uri: URI, file: e.target.files[0] });
-    //
-    console.clear();
-    console.log("file", image.file);
-  };
+  const loadImage = (e) =>
+    setImage({
+      uri: URL.createObjectURL(e.target.files[0]),
+      file: e.target.files[0]
+    });
 
   // Load a picture
   const handleSubmit = async (img_) => {
+    setIsLoading(true);
     let fileUploaded = null;
     if (img_.file) {
       fileUploaded = await handleUploadFiles.uploadFile(img_);
@@ -31,29 +34,37 @@ const Home = () => {
 
       // call API-IA
       if (url) {
-        //
-        // const resPrediction =
+        setDataForm({ ...dataForm, linkImg: url });
+        createPrediction(url);
       }
-      console.clear();
+      //
       console.log("url", url);
-      // setFormData({ ...formData, avatar: url });
     }
+    setIsLoading(false);
   };
 
   const createPrediction = async () => {
-    const res = await postAPI("");
+    // const res = await postAPI("");
+    const res = {
+      success: true,
+      prediction: "glasses"
+    };
+
     if (res) {
       //
+      //   setImage({ uri: null, file: null });
+      setDataForm({ ...dataForm, prediction: res.prediction });
     }
   };
   useEffect(() => {
     //
-  }, []);
+  }, [dataForm.linkImg]);
 
   return (
     <div className='mainHome'>
       <div className='contentResume'>
-        Home udshcbs chuysdv jdsuyvisd udshcbs chuysdv jdsuyvisd
+        {/* resume your model here */}
+        Place the details of your model here
       </div>
       <div className='contentChargeFile-res'>
         <div className='firstFlex'>
@@ -62,24 +73,32 @@ const Home = () => {
           <div className='contentInputPicture'>
             <div className='contentInput'>
               <div>
-                {/* <label> Resultat attendu </label> */}
                 <input
                   type='text'
                   className='inputText'
                   placeholder=' Entrez le resultat attendu, ex  glasses'
+                  onChange={(e) => {
+                    setDataForm({ ...dataForm, attendu: e.target.value });
+                  }}
                 />
               </div>
               <div>
-                {/* <label> Entrer le lien de la photo </label> */}
-                <input type='file' className='' onChange={() => loadImage()} />
+                <input
+                  type='file'
+                  multiple={false}
+                  onChange={(e) => loadImage(e)}
+                />
               </div>
               <div>
                 <button
                   type='button'
                   className='btn btn-primary'
                   onClick={() => handleSubmit(image)}
+                  disabled={
+                    dataForm.attendu === "" || image.uri === null ? true : false
+                  }
                 >
-                  Charger
+                  {isLoading ? "Loading..." : "Charger"}
                 </button>
               </div>
             </div>
@@ -90,12 +109,17 @@ const Home = () => {
         </div>
         <div className='lastFlex photoLoaded'>
           <h2> Photo chargée </h2>
-          <img src={imgDev} alt='...' />
+          {image.uri !== null && <img src={image.uri} alt='...' />}
         </div>
         <div className='lastFlex'>
           <h2> Resultat de la prédiction </h2>
-          <h4> Resultat attendu </h4>
-          <h4> Resultat de la prediction </h4>
+          <h4>
+            Resultat attendu :{dataForm.prediction !== "" && dataForm.attendu}
+          </h4>
+          <h4>
+            Resultat de la prediction :
+            {dataForm.prediction !== "" && dataForm.prediction}
+          </h4>
         </div>
       </div>
     </div>
